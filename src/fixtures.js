0eb87bf167d70c8798bf8847225ef36c255140f3
Ulyana <buskoulya@gmail.com>
2018-04-01 20:45:50 +0300

    my first app react

---
+++ src/fixtures.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        "id": "1",
+        "date": "2016-06-09T15:03:23.000Z",
+        "title": "What is Frontend?",
+        "text": "Front-end or client-side development is a relatively obscure Internet discipline. Historically, this role has been known under several aliases, htmler, web designer, coder, frontender and so on, but its core functions remain the same while expanding with the progress of the Internet. It is a hinge role that requires both aesthetic sensitivity and programmatic rigor. To many people, client-side development might be perceived as 'making things pretty' and, while it is a valid comment since we do make things look good, as good-looking things sell better, there are many other technologies that fall within this field that might be usually overlooked. Here's an overview of some of them.",
+        "comments": [
+            {
+                "id": "qwert",
+                "user": "Ulyana Busko",
+                "text": "I like frontend :)"
+            },
+            {
+                "id": "tyuio",
+                "user": "Natasha Chobot",
+                "text": "I have a job!"
+            }
+        ]
+    },
+    {
+        "id": "2",
+        "date": "2016-06-08T15:03:23.000Z",
+        "title": "Do you like cats?",
+        "text": "Cats are very popular pets and there are many different breeds that all vary in size, colour and markings. An average cat reaches a length of 50-60 cm and weighs about 3-5 kg. The fur may be black, white, brown, grey, red, black and white, tabby (striped) or calico. Cats are very good hunters and use their strong, sharp claws and teeth to grab and hold their prey. In the wild, cats feed on mice, birds and other small animals.",
+        "comments": [
+            {
+                "id": "qwert",
+                "user": "Ulyana Busko",
+                "text": "I have a cat. Her name is Vosya!"
+            },
+            {
+                "id": "tyuio",
+                "user": "Natasha Chobot",
+                "text": "Ohh, they are so cute)"
+            }
+        ]
+    }
+]
